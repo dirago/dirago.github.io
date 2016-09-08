d2a34075a8f723ee3a4fc413f85247735eb51dcb
dirago <diragor@gmail.com>
2016-09-08 12:00:40 +0200

    header height increased

--- EcoNet/js/scroll.js
+++ EcoNet/js/scroll.js
@@ -2,12 +2,12 @@ $(window).on('scroll', function() {
     var header = $('header'),
         langMob = $('#lang-mob'),
         navMob = $('#nav-mob'),
-        header_height = header.outerHeight(),
+        header_height = header.outerHeight() / 2,
         cur_pos = $(this).scrollTop();
     if (cur_pos >= header_height) {
-        header.addClass('header-sm');
         langMob.addClass('lang-mob--sm');
         navMob.addClass('nav-mob--sm');
+        header.addClass('header-sm');
     } else {
         header.removeClass('header-sm');
         langMob.removeClass('lang-mob--sm');
